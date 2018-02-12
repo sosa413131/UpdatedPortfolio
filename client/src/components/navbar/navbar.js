f6e4f6b1790e44dadfaf2f2a03adc612720b6387
David W Sosa <sosa413131@yahoo.com>
2018-02-12 14:00:19 -0500

    added reset css and updated navbar and font-awesome icons in the jumbotron

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -21,21 +21,21 @@ class navbar extends Component {
                         <NavItem eventKey={2} href="#">
                             Portfolio
     </NavItem>
+                        <NavItem eventKey={3} href="#">
+                            Contact
+
+    </NavItem>
+
+                    </Nav>
+                    <Nav pullRight>
+
                         <NavDropdown eventKey={3} title="Social Media" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1}href="https://www.linkedin.com/in/david-w-sosa-19706238/">LinkedIn</MenuItem>
-                            <MenuItem eventKey={3.2}href="https://github.com/sosa413131">Github</MenuItem>
-                            <MenuItem eventKey={3.3}href="https://stackoverflow.com/users/8422298/sosa413131">Stack Overflow</MenuItem>
+                            <MenuItem eventKey={3.1} href="https://www.linkedin.com/in/david-w-sosa-19706238/">LinkedIn</MenuItem>
+                            <MenuItem eventKey={3.2} href="https://github.com/sosa413131">Github</MenuItem>
+                            <MenuItem eventKey={3.3} href="https://stackoverflow.com/users/8422298/sosa413131">Stack Overflow</MenuItem>
                             <MenuItem divider />
                             <MenuItem eventKey={3.3}>Email</MenuItem>
                         </NavDropdown>
-                    </Nav>
-                    <Nav pullRight>
-                        <NavItem eventKey={1} href="#">
-                       Contact
-    </NavItem>
-                        <NavItem eventKey={2} href="#">
-                            {/* Link Right */}
-    </NavItem>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
