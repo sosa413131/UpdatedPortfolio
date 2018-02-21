5f58e28e1550a729acf7f6bcb58b407bf2df3b2f
David W Sosa <sosa413131@yahoo.com>
2018-02-20 20:55:50 -0500

    commented out lorem ipsum

--- client/src/components/container/container.js
+++ client/src/components/container/container.js
@@ -10,7 +10,7 @@ const Container = () => {
             I graduated from Union College in Schenectady, NY with a degree in Managerial Economics and for the last 4 years I have been working at JP Morgan. More recently I have been enrolled in a certificate program for Full-Stack Web Development at Rutgers University. I enjoy solving complex problems and using big data to provide insight into consumer behavior.
 
             </Col>
-            <br/>
+            {/* <br/>
 
             <Col md={4} sm={12}>
                 Suspendisse potenti. Phasellus dapibus mi vel convallis ullamcorper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam pretium eu quam a fermentum. Mauris scelerisque sem nisl, nec faucibus dolor lobortis sagittis. Quisque convallis posuere odio, et gravida erat tempus in. Vestibulum feugiat tortor tincidunt euismod faucibus. Mauris dictum arcu at ante tempor luctus. Nunc at pharetra ante. Nam lacinia velit nisl, nec placerat dolor euismod eget. Praesent ac lectus eu sapien iaculis bibendum et sit amet felis. Pellentesque molestie at lorem id mattis. Mauris rhoncus erat vel erat sollicitudin laoreet. Nulla facilisi.
@@ -19,7 +19,7 @@ const Container = () => {
 
             <Col md={4} sm={12}>
                 Suspendisse potenti. Phasellus dapibus mi vel convallis ullamcorper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam pretium eu quam a fermentum. Mauris scelerisque sem nisl, nec faucibus dolor lobortis sagittis. Quisque convallis posuere odio, et gravida erat tempus in. Vestibulum feugiat tortor tincidunt euismod faucibus. Mauris dictum arcu at ante tempor luctus. Nunc at pharetra ante. Nam lacinia velit nisl, nec placerat dolor euismod eget. Praesent ac lectus eu sapien iaculis bibendum et sit amet felis. Pellentesque molestie at lorem id mattis. Mauris rhoncus erat vel erat sollicitudin laoreet. Nulla facilisi.
-            </Col>
+            </Col> */}
 
         </div>
     )
